9b6b0148abd3bfe02a19482b353a62acf79b1454
Ryan Domingue <rychdom+github@gmail.com>
2020-05-01 15:23:20 -0700

    ISSUE-2 Adds failing tests

--- src/getSelectorFromComponent.spec.js
+++ src/getSelectorFromComponent.spec.js
@@ -5,7 +5,7 @@ describe('Get a hashed class selector from a component', () => {
   test.each([
     ['alignleft', 'mock-one', '.mock-one__alignleft___1cXIA'],
     ['alignleft', 'mock-two', '.mock-two__alignleft___Lww3s'],
-    ['title', 'mock-one', '.mock-one__title___2kGA7 _typography__header-main___2IME8'],
+    ['title', 'mock-one', '.mock-one__title___2kGA7._typography__header-main___2IME8'],
     ['shared-name', 'mock-two', '.mock-two__shared-name___Kf9x0'],
   ])(
     'Gets the `%s` selector from `%s`',
